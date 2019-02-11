2049586dce5591288e4e1e46c4cc2593692385ac
Vatril <=>
2019-02-12 00:32:52 +0100

    extracted searchbar

---
+++ src/components/input/Searchbar.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+
+export default class Searchbar extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            value: ""
+        }
+    }
+
+    onChange(evt) {
+        this.setState({
+            value: evt.target.value
+        });
+    }
+
+    searchOnEnter(evt) {
+        if (evt.key === "Enter") {
+            this.search();
+        }
+    }
+
+    search() {
+        if(typeof this.props.onSearch === "function"){
+            this.props.onSearch(this.state.value)
+        }
+    }
+
+    render() {
+        return (
+            <div id="searchbar">
+                <input
+                    id="searchfield"
+                    type="search"
+                    placeholder="Search"
+                    value={this.state.value}
+                    onChange={evt => this.onChange(evt)}
+                    onKeyPress={evt => this.searchOnEnter(evt)}
+                />
+                <button onClick={evt => this.search()} id="searchbutton">Search</button>
+            </div>
+        )
+    }
+}
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react'
 import "./navbar.css"
+import Searchbar from '../input/Searchbar';
 
 export default class Navbar extends Component {
     render() {
         return (
             <div id="header">
                 <h1 id="namelogo">Flink</h1>
-                <div id="searchbar">
-                    <input id="searchfield" type="search" placeholder="Search" />
-                    <button id="searchbutton">Search</button>
-                </div>
+                <Searchbar onSearch={query => alert(query)}/>
                 <div id="navlinks">
                     <a href="/raffles" className="link">Raffles</a>
                     <a href="/login" className="link">Login</a>
