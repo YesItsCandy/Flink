3f23739dbf4e7eb262ad5f225a87d4bf74ed8359
Vatril <=>
2019-02-12 01:19:20 +0100

    added featured api

--- src/components/homescreen/Featured.js
+++ src/components/homescreen/Featured.js
@@ -1,19 +1,52 @@
 import React, { Component } from 'react'
 import "./featured.css"
+import Axios from 'axios';
 
 export default class Featured extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            loaded: false,
+            error: false,
+            featured: []
+        }
+    }
+
+    componentDidMount() {
+        Axios.get("/featured")
+            .then(res => {
+                this.setState({
+                    error:false,
+                    loaded: true,
+                    featured: res.data.slice(0,5)
+                })
+            }).catch(err => {
+                console.error(err)
+                this.setState({error:true})
+            })
+    }
+
+
     render() {
         return (
             <div id="featured">
                 <h1>Featured</h1>
                 <div className="image-gallary">
-                    <img className="preview-image" alt="placeholder" src="https://via.placeholder.com/350x150" />
-                    <img className="preview-image" alt="placeholder" src="https://via.placeholder.com/100x100" />
-                    <img className="preview-image" alt="placeholder" src="https://via.placeholder.com/300x150" />
-                    <img className="preview-image" alt="placeholder" src="https://via.placeholder.com/100x200" />
-                    <img className="preview-image" alt="placeholder" src="https://via.placeholder.com/200x300" />
+                    {this.state.featured.map((image, idx) => 
+                    <img 
+                    key={idx} 
+                    alt={image.name} 
+                    src={image.src}
+                    className="preview-image"
+                    />
+                    )}
                 </div>
+                {this.state.error && <h2>Something went wrong loading</h2>}
+                {!this.state.loaded && <h2>Loading Symbol</h2>}
             </div>
         )
+        //TODO add loading symbol
     }
 }
--- website.py
+++ website.py
@@ -7,7 +7,13 @@ app = Flask(__name__)
 @app.route('/featured')
 def featured():
     return jsonify([
-        
+        {"src":"https://via.placeholder.com/350x150", "name":"test1"},
+        {"src":"https://via.placeholder.com/200x200", "name":"test2"},
+        {"src":"https://via.placeholder.com/300x150", "name":"test3"},
+        {"src":"https://via.placeholder.com/100x200", "name":"test4"},
+        {"src":"https://via.placeholder.com/200x300", "name":"test5"},
+        {"src":"https://via.placeholder.com/200x300", "name":"test6"},
+        {"src":"https://via.placeholder.com/200x300", "name":"test7"}
     ])
 
 
