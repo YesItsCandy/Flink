6f545e2309a6e9a83aaab82a992adf0ff3319ccd
Vatril <nikolaisummers@gmail.com>
2019-02-13 00:16:07 +0100

    added welcome message

--- src/components/homescreen/Homescreen.js
+++ src/components/homescreen/Homescreen.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
 import "./homescreen.css"
 import Featured from './Featured';
+import { UserContext } from '../../Context';
 
 export default class Homescreen extends Component {
     render() {
         return (
             <>
                 <div id="welcometext">
-                    <h1>Welcome</h1>
+                    <UserContext.Consumer>
+                        {uc =><h1>Welcome {uc.user?uc.user.username:""}</h1>}
+                    </UserContext.Consumer>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco
@@ -16,7 +19,7 @@ export default class Homescreen extends Component {
                       eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
                 sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div>
-                <Featured/>
+                <Featured />
             </>
         )
     }
