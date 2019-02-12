c27064a6e8fcc63202ad70bc842b631132da0db2
Vatril <=>
2019-02-12 16:39:29 +0100

    added home link

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,8 +6,8 @@ export default class Navbar extends Component {
     render() {
         return (
             <div id="header">
-                <h1 id="namelogo">Flink</h1>
-                <Searchbar onSearch={query => alert(query)}/>
+                <a className="link" href="/"><h1 id="namelogo">Flink</h1></a>
+                <Searchbar onSearch={query => alert(query)} />
                 <div id="navlinks">
                     <a href="/raffles" className="link">Raffles</a>
                     <a href="/login" className="link">Login</a>
